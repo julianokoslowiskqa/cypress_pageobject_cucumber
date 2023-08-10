a7a9a397d5d7b01af7c1a85cebf0941d948c9926
julianokoslowiskqa <julianokoslowisk@gmail.com>
2023-08-10 01:59:24 -0300

    update ci/cs

--- cypress/support/Elements/signElements.js
+++ cypress/support/Elements/signElements.js
@@ -3,9 +3,7 @@ class SignElements {
     inputEmail = () => {return '.col-md-6 input[name="email"]'}
     InputPassword = () => {return 'input[type="password"]'}
     submitButton = () => {return '#submit-login'}
-
     textConfirm = () => {return 'h1'}
-
     alertText = () => {return 'li[class="alert alert-danger"]'}
 }
 
